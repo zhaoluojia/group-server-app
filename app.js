34b16500da4ce80758e4e827ab8db044472bff64
Elena Zhao <elena@Elenas-MacBook-Air.local>
2022-11-26 17:53:55 -0800

    add register

--- app.js
+++ app.js
@@ -1,8 +1,12 @@
+import mongoose from "mongoose";
 import express from 'express';
 import cors from 'cors';
+import UsersController from "./users/users-controller.js";
+
+mongoose.connect('mongodb+srv://luojiazhao:wWvgUnODWbFEeIn7@yealp-db.iarbu28.mongodb.net/yealp');
 
 const app = express();
 app.use(cors());
 app.use(express.json());
-app.get('/', (req, res) => {res.send('Group Project')})
+UsersController(app);
 app.listen(4000);
---
+++ users/users-schema.js
@@ -0,0 +1,22 @@
+import mongoose from "mongoose";
+
+// User (including different roles. Use property “role” to distinguish)
+const userSchema = mongoose.Schema({
+  firstName: {type: String, required: true},
+  lastName: {type: String, required: true},
+  username: {type: String, required: true, unique: true},
+  email: {type: String, required: true},
+  phone: {type: String, required: true},
+  profilePicture: {type: String, required: true, default: "https://user-images.githubusercontent.com/15254181/203483222-a01e1942-19e4-49c8-b3e3-41dd0ce8b3de.jpg"},
+  bannerPicture: {type: String, required: true, default: "https://user-images.githubusercontent.com/15254181/203613376-d7c29b41-9deb-4fce-9ebd-0c0a5f5cf44b.jpg"},
+  bio: {type: String, default: ""}, // admin don't have bio
+  role: {type: String, enum: ['CUSTOMER', 'ADMIN', 'OWNER'], required: true},
+  location: {type: String, required: true},
+  dateOfBirth: {type: String, required: true},
+  dateJoined: {type: String, required: true, default: (new Date()).toLocaleDateString()},
+  password: {type: String, required: true},
+  followersCount: {type: Number, default: 0}, // admin and owners don't have followers and people to follow
+  followingCount: {type: Number, default: 0}, // admin and owners don't have followers and people to follow
+}, {collection: 'users', versionKey: false});
+
+export default userSchema
