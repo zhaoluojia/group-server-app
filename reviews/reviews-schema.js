1249e025caf22ed341abdb19ca8217f4aeca3e87
Wanda Wang <chuwang@wustl.edu>
2022-11-26 22:00:32 -0800

    Add findUserByID and update schema dates

--- reviews/reviews-schema.js
+++ reviews/reviews-schema.js
@@ -3,7 +3,7 @@ import mongoose from "mongoose";
 const reviewSchema = mongoose.Schema({
   rating: {type: Number, enum: [1, 2, 3, 4, 5], required: true},
   comment: {type: String, required: true, default: ''},
-  postedDate: {type: String, required: true},
+  postedDate: {type: Date, required: true},
   restaurantID: {type: String, required: true},
   customerID: {type: String, required: true},
   ownerReply: {type: String}, // not required
