6eca3ec8eaecf8ee6a71932adcc057f4bfcbb470
Wanda Wang <chuwang@wustl.edu>
2022-11-28 10:19:05 -0800

    Update restaurant schema to add yelpID

--- resteaurants/restaurants-schema.js
+++ resteaurants/restaurants-schema.js
@@ -2,6 +2,7 @@ import mongoose from "mongoose";
 
 const restaurantsSchema = mongoose.Schema({
   name: {type: String, required: true},
+  yelpID: {type: String, required: true},
   image_url: {type: String},
   category: {type: String, required: true},
   url: {type: String, required: true},
