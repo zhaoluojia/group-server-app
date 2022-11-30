7405fae6b3c1c3c09c36e4fc6d1ccb401fcc44d9
Heyang Chen <yuelin@iMac.local>
2022-11-29 22:19:24 -0800

    11/29

--- reviews/reviews-controller.js
+++ reviews/reviews-controller.js
@@ -12,8 +12,30 @@ const findReviewByRestaurantID =async (req, res) => {
   res.json(reviews)
 }
 
+const createReview = async (req, res) => {
+  const newReview = req.body;
+  newReview.ownerReply = '';
+  const insertedReview = await reviewsDao.createReview(newReview);
+  res.json(insertedReview);
+}
+
+const findAllReviews = async (req, res) => {
+  const reviews = await reviewsDao.findAllReviews();
+  res.json(reviews)
+};
+
+const deleteReviewByID = async (req, res) => {
+  const rid = req.params.rid;
+  const status = await reviewsDao.deleteReviewByID(rid);
+  res.send(status);
+}
+
+
 export default (app) => {
+  app.get('/api/reviews', findAllReviews);
   app.get('/api/reviews/owners/:oid', findAllReviewsByUserID);
   app.get('/api/reviews/restaurant/:rid', findReviewByRestaurantID);
+  app.post('/api/reviews', createReview);
+  app.delete('/api/reviews/:rid', deleteReviewByID);
 };
 
