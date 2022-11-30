b2298c83c14b97947b053124bf3c017d26c84371
Wanda Wang <chuwang@wustl.edu>
2022-11-30 11:34:23 -0800

    Update advertisement-dao to sort by posted date

--- advertisements/advertisements-dao.js
+++ advertisements/advertisements-dao.js
@@ -1,8 +1,8 @@
 import advertisementsModel from "./advertisements-model.js";
 
-export const findAllAdvertisementsByOwnerID = (oid) => advertisementsModel.find({ownerID: oid});
-export const findAllAdvertisements = () => advertisementsModel.find();
-export const findAdvertisementByID = (aid) => advertisementsModel.findById(aid);
+export const findAllAdvertisementsByOwnerID = (oid) => advertisementsModel.find({ownerID: oid}).sort({postedDate: "desc"});
+export const findAllAdvertisements = () => advertisementsModel.find().sort({postedDate: "desc"});
+export const findAdvertisementByID = (aid) => advertisementsModel.findById(aid).sort({postedDate: "desc"});
 export const createAdvertisement = (ad) => advertisementsModel.create(ad);
 export const deleteAdvertisementByID = (aid) => advertisementsModel.findByIdAndDelete(aid);
 export const updateAdvertisementByID = (aid, content) => advertisementsModel.updateOne({_id: aid}, {$set: content});
