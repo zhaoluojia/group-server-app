37af342dffa93b188fce270d9926c0b011a95b1b
Wanda Wang <chuwang@wustl.edu>
2022-12-01 12:11:44 -0800

    Fix yelp cors issue

--- resteaurants/restaurants-controller.js
+++ resteaurants/restaurants-controller.js
@@ -1,4 +1,5 @@
 import * as restaurantsDao from './restaurants-dao.js';
+import yelp from 'yelp-fusion';
 
 const findAllRestaurants = async (req, res) => {
   const restaurants = await restaurantsDao.findAllRestaurants();
@@ -36,11 +37,33 @@ const findRestaurantsByCategory = async (req, res) => {
   res.json(restaurant);
 }
 
+// Yelp related search are below
+const apiKey = 'xFy8A_Q4TV1AU_eFIvAp2M36xLFgIRTvwQgi2HZE2wF9iZxV1548dIgZ2IER6HqqJptx8iqbQuhcU7xvxFGew7fMMMbY8J3WqMEK79sAvxQgc-bU-EZiKvAQTRZ4Y3Yx';
+const client = yelp.client(apiKey);
+const findRestaurantsByRequestUsingYF = async (req, res) => {
+  res.header("Access-Control-Allow-Origin", "*");
+  res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+
+  const searchRequest = {
+    term: req.params.rname,
+    location: req.params.rlocation
+  }
+  client.search(searchRequest).then(response => {
+    const firstResult = response.jsonBody;
+    res.json(firstResult);
+  }).catch(e => {
+    res.json(e);
+  });
+}
+
+
+
 export default (app) => {
   app.get('/api/restaurants/category/:c', findRestaurantsByCategory);
   app.get('/api/restaurants/name/:rName', findRestaurantsByRestaurantName);
   app.get('/api/restaurants/:rid', findRestaurantByRestaurantID);
   app.get('/api/restaurants', findAllRestaurants);
+  app.get('/api/restaurants/search/:rname/:rlocation', findRestaurantsByRequestUsingYF); // YelpFusionAPI
   app.get('/api/restaurants/owners/:oid', findRestaurantsByOwnerID);
   app.put('/api/restaurants/disconnect/:rid', disConnectOwnerAndRestaurant);
 };
