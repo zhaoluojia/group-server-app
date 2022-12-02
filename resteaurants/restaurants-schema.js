bf13b74cbcdb904e10e8cb9d6f9504f2d4a592df
Wanda Wang <chuwang@wustl.edu>
2022-12-01 20:26:13 -0800

    Removed duplicate yelId in restaurantSchema, remove reviews array since we don't use that

--- resteaurants/restaurants-schema.js
+++ resteaurants/restaurants-schema.js
@@ -2,14 +2,12 @@ import mongoose from "mongoose";
 
 const restaurantsSchema = mongoose.Schema({
   name: {type: String, required: true},
-  yelpID: {type: String, required: true},
   image_url: {type: String},
   category: {type: String, required: true},
   url: {type: String, required: true},
   price: {type: String, required: true},
   display_phone: {type: String, required: true},
   owners: {type: [String], required: true}, // could have many owners, so use array here
-  reviews: {type: [String], required: true}, // array of reviewID
   yelpID: {type: String, required: true},
 }, {collection: 'restaurants', versionKey: false});
 
