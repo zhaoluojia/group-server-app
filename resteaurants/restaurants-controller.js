f70b669269061ea0e163901ba6b2dcf4be7a333a
Wanda Wang <chuwang@wustl.edu>
2022-12-03 15:29:17 -0800

    Update for createRestaurant

--- resteaurants/restaurants-controller.js
+++ resteaurants/restaurants-controller.js
@@ -21,13 +21,15 @@ const findRestaurantsByOwnerID = async (req, res) => {
 const disConnectOwnerAndRestaurant = async (req, res) => {
   const rid = req.params.rid;
   const ownerID = req.body;
-  const status = await restaurantsDao.disConnectOwnerAndRestaurant(ownerID, rid);
+  const status = await restaurantsDao.disConnectOwnerAndRestaurant(ownerID,
+      rid);
   res.send(status);
 }
 
 const findRestaurantsByRestaurantName = async (req, res) => {
   const rName = req.params.name;
-  const restaurant = await restaurantsDao.findRestaurantsByRestaurantName(rName);
+  const restaurant = await restaurantsDao.findRestaurantsByRestaurantName(
+      rName);
   res.json(restaurant);
 }
 
@@ -38,11 +40,13 @@ const findRestaurantsByCategory = async (req, res) => {
 }
 
 // Yelp related search are below
-const apiKey = 'xFy8A_Q4TV1AU_eFIvAp2M36xLFgIRTvwQgi2HZE2wF9iZxV1548dIgZ2IER6HqqJptx8iqbQuhcU7xvxFGew7fMMMbY8J3WqMEK79sAvxQgc-bU-EZiKvAQTRZ4Y3Yx';
+// const apiKey = 'xFy8A_Q4TV1AU_eFIvAp2M36xLFgIRTvwQgi2HZE2wF9iZxV1548dIgZ2IER6HqqJptx8iqbQuhcU7xvxFGew7fMMMbY8J3WqMEK79sAvxQgc-bU-EZiKvAQTRZ4Y3Yx';
+const apiKey = 'EFxgaP5x-KP9rOD37LBwEovvXmf4gt6WYyMEk7FtsJLpAntnxADfT728c9oPu3-0NvSfJkW7BqVnVgjWur0uWXp6XuRzWwg9RE7xSaDm9B5aWZ2TxjboeVT3gcSKY3Yx';
 const client = yelp.client(apiKey);
 const findRestaurantsByRequestUsingYF = async (req, res) => {
   res.header("Access-Control-Allow-Origin", "*");
-  res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+  res.header("Access-Control-Allow-Headers",
+      "Origin, X-Requested-With, Content-Type, Accept");
 
   const searchRequest = {
     term: req.params.rname,
@@ -71,7 +75,6 @@ const findRestaurantsByRequestIDUsingYF = async (req, res) => {
 }
 // Yelp related search ends here
 
-
 const findRestaurantByYelpId = async (req, res) => {
   const yid = req.params.yid;
   const restaurant = await restaurantsDao.findRestaurantByYelpId(yid);
@@ -80,13 +83,8 @@ const findRestaurantByYelpId = async (req, res) => {
 
 const createRestaurant = async (req, res) => {
   const newRestaurant = req.body;
-  // const yid = req.params.yid;
-  // const restaurant = restaurantsDao.findRestaurantByYelpId(yid);
-  // if(restaurant === null) {
-  const insertedRestaurant = await restaurantsDao.createRestaurant(newRestaurant);
-  res.json(insertedRestaurant);
-  // }
-
+  const inserted = await restaurantsDao.createRestaurant(newRestaurant);
+  res.json(inserted);
 }
 
 export default (app) => {
@@ -94,8 +92,10 @@ export default (app) => {
   app.get('/api/restaurants/name/:rName', findRestaurantsByRestaurantName);
   app.get('/api/restaurants/:rid', findRestaurantByRestaurantID);
   app.get('/api/restaurants', findAllRestaurants);
-  app.get('/api/restaurants/search/yelpid/:yid', findRestaurantsByRequestIDUsingYF) // YelpFusionAPI
-  app.get('/api/restaurants/search/:rname/:rlocation', findRestaurantsByRequestUsingYF); // YelpFusionAPI
+  app.get('/api/restaurants/search/yelpid/:yid',
+      findRestaurantsByRequestIDUsingYF) // YelpFusionAPI
+  app.get('/api/restaurants/search/:rname/:rlocation',
+      findRestaurantsByRequestUsingYF); // YelpFusionAPI
   app.get('/api/restaurants/owners/:oid', findRestaurantsByOwnerID);
   app.put('/api/restaurants/disconnect/:rid', disConnectOwnerAndRestaurant);
   app.get('/api/restaurants/yelpID/:yid', findRestaurantByYelpId);
--- resteaurants/restaurants-schema.js
+++ resteaurants/restaurants-schema.js
@@ -3,12 +3,21 @@ import mongoose from "mongoose";
 const restaurantsSchema = mongoose.Schema({
   name: {type: String, required: true},
   image_url: {type: String},
-  category: {type: String, required: true},
-  url: {type: String, required: true},
-  price: {type: String, required: true},
-  display_phone: {type: String, required: true},
+  category: {type: String},
+  url: {type: String},
+  price: {type: String},
+  display_phone: {type: String},
   owners: {type: [String], required: true}, // could have many owners, so use array here
-  yelpID: {type: String, required: true},
+  yelpID: {
+    type: String, unique: true, required: true,
+    validate: {
+      validator: async function() {
+        let item = await this.constructor.findOne({yelpID: this.yelpID})
+        return Boolean(!item)
+      }
+    }
+
+  },
 }, {collection: 'restaurants', versionKey: false});
 
 export default restaurantsSchema;
